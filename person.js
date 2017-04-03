ef75afd5f09ba0c189f7dd37abbdc7b4ec800528
mlimberg <limbergmike@gmail.com>
2017-04-03 11:09:25 -0600

    Add person methods

--- person.js
+++ person.js
@@ -1,8 +1,16 @@
 export default class Person {
   constructor() {
-    // each person starts out with 5 dollars
     this.state = {
-      credits = 500      
+      credits: 500
     }
+  }
+
+  insertCredit(value) {
+    this.state -= value;
+    return value
+  }
+
+  makeSelection(choice) {
+    return choice;
   }
 }
