1d086620732481e2ee7716488494a208fcee40c1
mlimberg <limbergmike@gmail.com>
2017-04-04 19:31:55 -0600

    Add vending machine methods and tests

--- tests/vendingMachineTest.js
+++ tests/vendingMachineTest.js
@@ -3,30 +3,96 @@ require('babel-core/register')({
 });
 
 const assert = require('chai').assert
-const VendingMachine = require('../vendingMachine').default
-const Person = require('../person').default
+const VendingMachine = require('../lib/vendingMachine').default
+const Person = require('../lib/person').default
 
-describe('Elevator', function() {
+describe('Vending Machine', function() {
   const vendingMachine = new VendingMachine()
-  const alex = new Person("Alex", 100)
+  const bimby = new Person("Alex", 100)
 
   afterEach(function() {
     vendingMachine.reset();
   });
 
-  xit('should bring a rider to a floor above their current floor', () => {
-    // Assert the current status of the vendingMachine is idle
-    assert.equal(vendingMachine.status, 'idle')
+  it('should accept credits', () => {
+    assert.equal(vendingMachine.state.status, 'idle')
 
-    // Alex inserts a dollar into the vending machine
-    vendingMachine.insertCredit(alex, 100)
+    vendingMachine.addCredit(bimby.insertCredits(100))
 
-    // Assert the current status of the vendingMachine is 'credited' after credits inserted
-    assert.equal(vendingMachine.status, 'credited')
-    // Assert the total number of credits is 100 cents ($1.00) after credits inserted
-    assert.equal(vendingMachine.credits, 100)
-    // Assert the total number of change is 0 cents ($0.00) before selection is made
-    assert.equal(vendingMachine.change, 0)
+    assert.equal(vendingMachine.state.status, 'credited')
+    assert.equal(vendingMachine.state.credits, 100)
+    // assert.equal(vendingMachine.state.change, 0)
   });
 
+  it('should save selections and credit if 100 credits are added', () => {
+    assert.equal(vendingMachine.state.status, 'idle');
+
+    vendingMachine.addCredit(bimby.insertCredits(100));
+    vendingMachine.setSelection(bimby.makeSelection('A1'));
+
+    assert.equal(vendingMachine.state.selection, 'A1');
+    assert.equal(vendingMachine.state.credits, 100);
+
+  })
+
+  it('should save selections and credit if 50 credits are added', () => {
+    assert.equal(vendingMachine.state.status, 'idle');
+
+    vendingMachine.addCredit(bimby.insertCredits(50));
+    vendingMachine.setSelection(bimby.makeSelection('A2'));
+
+    assert.equal(vendingMachine.state.selection, 'A2');
+    assert.equal(vendingMachine.state.credits, 50);
+
+  })
+
+  it('should prompt an error if there are not enough credits', () => {
+    vendingMachine.addCredit(bimby.insertCredits(50))
+    assert.equal(vendingMachine.state.credits, 50)
+    vendingMachine.setSelection(bimby.makeSelection('A1'))
+  })
+
+
+
 });
+
+describe('vendingMachine methods', () => {
+  const vendingMachine = new VendingMachine()
+
+  afterEach(function() {
+    vendingMachine.reset();
+  });
+
+  it('should have an addCredit() method', () => {
+    vendingMachine.addCredit(100)
+    assert.equal(vendingMachine.state.credits, 100)
+    assert.equal(vendingMachine.state.status, 'credited')
+  })
+
+  it('should have an setSelection() method', () => {
+    vendingMachine.treats = {
+      B5: [{name: 'cheetohs', price: 75}]
+    }
+    vendingMachine.setSelection('B5')
+    assert.equal(vendingMachine.state.selection, 'B5')
+  })
+
+  xit('should have an checkCredits() method', () => {
+
+    vendingMachine.checkCredits()
+  })
+
+  it('should have an notEnoughCredits() method', () => {
+    vendingMachine.notEnoughCredits();
+    assert.equal(vendingMachine.state.status, 'more credits needed')
+  })
+
+  xit('should have an dispenseTreat() method', () => {
+
+  })
+
+  xit('should have an giveChange() method', () => {
+
+  })
+
+})
